0dc0027bc3a61710d1267dad800de7093aa61b5f
Sam Blackburn <sam_blackburn@pobox.com>
2020-08-02 02:30:07 -0400

    add_to_unique_sorted_vector() refactor
    Replaced std::equal_range() with std::lower_bound()

--- INCLUDE/stl_unique.h
+++ INCLUDE/stl_unique.h
@@ -4,82 +4,30 @@
 
 #define STL_STRING_UNIQUE_SORTED_VECTOR_HEADER_FILE
 
-using WSTRING_CONST_ITERATOR = std::vector<std::wstring>::const_iterator;
-using WSTRING_CONST_ITERATOR_PAIR = std::pair<WSTRING_CONST_ITERATOR, WSTRING_CONST_ITERATOR>;
-
-#if defined( WE_ARE_BUILDING_WFC_ON_UNIX )
-inline _Check_return_ bool it_wasnt_found(_In_ WSTRING_CONST_ITERATOR_PAIR const& p) noexcept
-#else
-inline constexpr _Check_return_ bool it_wasnt_found(_In_ WSTRING_CONST_ITERATOR_PAIR const& p) noexcept
-#endif
-{
-    return((p.first == p.second) ? true : false);
-}
-
-#if defined( WE_ARE_BUILDING_WFC_ON_UNIX )
-inline _Check_return_ bool it_was_found(_In_ WSTRING_CONST_ITERATOR_PAIR const& p) noexcept
-#else
-inline constexpr _Check_return_ bool it_was_found(_In_ WSTRING_CONST_ITERATOR_PAIR const& p) noexcept
-#endif
-{
-    return((p.first not_eq p.second) ? true : false);
-}
-
 inline _Check_return_ SSIZE_T add_to_unique_sorted_vector(_In_ std::wstring_view value_to_add, _Inout_ std::vector<std::wstring>& values) noexcept
 {
-    auto const p = std::equal_range(std::cbegin(values), std::cend(values), value_to_add);
+    auto const lower = std::lower_bound(std::cbegin(values), std::cend(values), value_to_add);
+    auto const return_value = std::distance(std::cbegin(values), lower);
 
-    if (it_was_found(p) == false)
+    if (not (lower not_eq std::cend(values) and value_to_add.compare(*lower) == I_AM_EQUAL_TO_THAT))
     {
-        SSIZE_T const return_value = std::distance(std::cbegin(values), p.second);
-
-        values.emplace(p.second, value_to_add);
-
-        // Return the index where we inserted
-        return(return_value);
+        values.emplace(lower, value_to_add);
     }
 
-    // Return the index of the existing entry
-    return(std::distance(std::cbegin(values), p.first));
-}
-
-using STRING_CONST_ITERATOR = std::vector<std::string>::const_iterator;
-using STRING_CONST_ITERATOR_PAIR = std::pair<STRING_CONST_ITERATOR, STRING_CONST_ITERATOR>;
-
-#if defined( WE_ARE_BUILDING_WFC_ON_UNIX )
-static inline _Check_return_ bool it_wasnt_found(_In_ STRING_CONST_ITERATOR_PAIR const& p) noexcept
-#else
-static inline constexpr _Check_return_ bool it_wasnt_found(_In_ STRING_CONST_ITERATOR_PAIR const& p) noexcept
-#endif
-{
-    return((p.first == p.second) ? true : false);
-}
-
-#if defined( WE_ARE_BUILDING_WFC_ON_UNIX )
-static inline _Check_return_ bool it_was_found(_In_ STRING_CONST_ITERATOR_PAIR const& p) noexcept
-#else
-static inline constexpr _Check_return_ bool it_was_found(_In_ STRING_CONST_ITERATOR_PAIR const& p) noexcept
-#endif
-{
-    return((p.first not_eq p.second) ? true : false);
+    return(return_value);
 }
 
 inline _Check_return_ SSIZE_T add_to_unique_sorted_vector(_In_ std::string_view value_to_add, _Inout_ std::vector<std::string>& values) noexcept
 {
-    auto const p = std::equal_range(std::cbegin(values), std::cend(values), value_to_add);
+    auto const lower = std::lower_bound(std::cbegin(values), std::cend(values), value_to_add);
+    auto const return_value = std::distance(std::cbegin(values), lower);
 
-    if (it_was_found(p) == false)
+    if (not (lower not_eq std::cend(values) and value_to_add.compare(*lower) == I_AM_EQUAL_TO_THAT))
     {
-        auto const return_value = std::distance(std::cbegin(values), p.second);
-
-        values.emplace(p.second, value_to_add);
-
-        // Return the index where we inserted
-        return(return_value);
+        values.emplace(lower, value_to_add);
     }
 
-    // Return the index of the existing entry
-    return(std::distance(std::cbegin(values), p.first));
+    return(return_value);
 }
 
 inline _Check_return_ bool compare_strings_ignoring_case(_In_ std::string_view left, _In_ std::string_view right) noexcept
@@ -87,22 +35,17 @@ inline _Check_return_ bool compare_strings_ignoring_case(_In_ std::string_view l
     return(compare_no_case(left, right) < I_AM_EQUAL_TO_THAT);
 }
 
-inline _Check_return_ SSIZE_T add_to_unique_sorted_vector_ignore_case(_In_ std::string const& value_to_add, _Inout_ std::vector<std::string>& values) noexcept
+inline _Check_return_ SSIZE_T add_to_unique_sorted_vector_ignore_case(_In_ std::string_view value_to_add, _Inout_ std::vector<std::string>& values) noexcept
 {
-    auto const p = std::equal_range(std::cbegin(values), std::cend(values), value_to_add, compare_strings_ignoring_case);
+    auto const lower = std::lower_bound(std::cbegin(values), std::cend(values), value_to_add, compare_strings_ignoring_case);
+    auto const return_value = std::distance(std::cbegin(values), lower);
 
-    if (it_was_found(p) == false)
+    if (not (lower not_eq std::cend(values) and value_to_add.compare(*lower) == I_AM_EQUAL_TO_THAT))
     {
-        auto const return_value = std::distance(std::cbegin(values), p.second);
-
-        values.emplace(p.second, value_to_add);
-
-        // Return the index where we inserted
-        return(return_value);
+        values.emplace(lower, value_to_add);
     }
 
-    // Return the index of the existing entry
-    return(std::distance(std::cbegin(values), p.first));
+    return(return_value);
 }
 
 inline _Check_return_ bool contains_no_case_sorted(_In_ std::vector<std::string> const& s, _In_z_ char const* the_string) noexcept
@@ -114,9 +57,9 @@ inline _Check_return_ bool contains_no_case_sorted(_In_ std::vector<std::string>
 
     std::string_view right_hand_side(the_string);
 
-    auto const p = std::equal_range(std::cbegin(s), std::cend(s), right_hand_side, compare_strings_ignoring_case);
+    auto const lower = std::lower_bound(std::cbegin(s), std::cend(s), right_hand_side, compare_strings_ignoring_case);
 
-    return(it_was_found(p));
+    return (lower not_eq std::cend(s) and right_hand_side.compare(*lower) == I_AM_EQUAL_TO_THAT);
 }
 
 inline _Check_return_ bool contains(_In_ std::vector<std::string> const& s, _In_z_ char const* the_string) noexcept
