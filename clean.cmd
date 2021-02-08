d7f621b5b1eeeb3d1f73402d55a06f4321abd811
Sam Blackburn <sam_blackburn@pobox.com>
2021-02-08 09:59:24 -0500

    Update build scripts
    I installed VS Pro

--- clean.cmd
+++ clean.cmd
@@ -4,10 +4,12 @@ if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\common7\ide\devenv.ex
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\common7\ide\devenv.exe" set VisualStudioVersion=14.0
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" set VisualStudioVersion=15.0
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" set VisualStudioVersion=16.0
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" set VisualStudioVersion=16.1
 :vsversionset
 
 rem by default pick msbuild based on VisualStudioVersion
 set _msbuildexe="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
+if "%VisualStudioVersion%" == "16.1" set _msbuildexe="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\amd64\MSBuild.exe"
 if "%VisualStudioVersion%" == "16.0" set _msbuildexe="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MSBuild.exe"
 if "%VisualStudioVersion%" == "15.0" set _msbuildexe="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 if "%VisualStudioVersion%" == "14.0" set _msbuildexe="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
